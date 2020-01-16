80c000365daeb889eedaaeeca152db7ed5eff9e1
Harlan Brown <hbrown@nuxeo.com>
2020-01-16 16:44:56 -0600

    add example mail function

--- src/main/java/org/nuxeo/sample/MyFancyBoxBean.java
+++ src/main/java/org/nuxeo/sample/MyFancyBoxBean.java
@@ -17,6 +17,7 @@ import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 
 @Name("myFancyBox")
 @Scope(ScopeType.EVENT)
@@ -27,11 +28,17 @@ public class MyFancyBoxBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private static final String OPERATION_NAME = "MyChain";
+    private static final String MAIL_OPERATION_NAME = "MyChain2";
 
     protected String assignee;
 
+    protected String addressee;
+
     @In(create = true, required = false)
     CoreSession documentManager;
+
+    @In(create = true, required = false)
+    protected transient NavigationContext navigationContext;
 
     public void assign() {
         try {
@@ -59,6 +66,36 @@ public class MyFancyBoxBean implements Serializable {
 
     public void setAssignee(String assignee) {
         this.assignee = assignee;
+    }
+
+    public void send() {
+        try {
+            AutomationService as = Framework.getLocalService(AutomationService.class);
+            OperationContext ctx = new OperationContext(documentManager);
+            Map<String, Object> params = new HashMap<String, Object>();
+
+            ctx.setInput(navigationContext.getCurrentDocument());
+
+            // the selection from the fancy box can be placed in a context variable
+            ctx.put("addressee", addressee);
+            
+            // or the parameter passed to the chain which does the assignment
+            params.put("addressee", addressee);
+
+            Object result = as.run(ctx, MAIL_OPERATION_NAME, params);
+        } catch (InvalidChainException e) {
+            log.error("Unable to resolve reference", e);
+        } catch (OperationException e) {
+            log.error("Unable to resolve reference", e);
+        }
+    } 
+
+    public String getAddressee() {
+        return addressee;
+    }
+
+    public void setAddressee(String addressee) {
+        this.addressee = addressee;
     }
 
 }    
